1bdd0ee90d9eff025005f718d1ec638a1cdad0bb
talha529 <talhaabid560@gmail.com>
2026-09-07 16:48:19 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BallPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+
         appBar: AppBar(
           backgroundColor: Colors.grey,
-          title: Center(child: Text("Magic8 Ball")),
-
-
+          title: Center(child: Text("Ask me anything")),
         ),
 
+        body: const Ball(),
+
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Image.asset("images/ball1.png"),
       ),
     );
   }
